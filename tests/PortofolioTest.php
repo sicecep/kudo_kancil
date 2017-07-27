745b9ddcf483108d2d89780feab0e67843ca06c7
Muhammad Ihsan <muhammad.ihsan@kudo.co.id>
2017-07-27 16:09:40 +0700

    add portofolio

--- tests/PortofolioTest.php
+++ tests/PortofolioTest.php
@@ -10,6 +10,17 @@ class PortofolioTest extends TestCase
     public function testPortofolio()
     {
         $this->visit('/')
-             ->see('contact-me-link');
+        ->see('contact-me-link')
+        ->click('contact-me-link')
+        ->seePageIs('/about-us');
     }
+
+    public function testPingPortofolio()
+    {
+        $this->visit('/')
+        ->see('ping-challenge-link')
+        ->click('ping-challenge-link')
+        ->seePageIs('/ping');
+    }
+
 }
