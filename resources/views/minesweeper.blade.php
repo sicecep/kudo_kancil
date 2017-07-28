66e0794de300e4b6d0d67f1006c5eb8bef4234de
Muhammad Ihsan <muhammad.ihsan@kudo.co.id>
2017-07-28 11:38:04 +0700

    add unit test minesweeper

--- resources/views/minesweeper.blade.php
+++ resources/views/minesweeper.blade.php
@@ -2,6 +2,19 @@
 <head>
 </head>
 <body>
-      Minesweeper
+      <title id="title">
+          Minesweeper
+      </title>
+      <table id="title">
+          @for($i=1;$i<=8;$i++)
+            <tr>
+                @for($j=1;$j<=8;$j++)
+                  <td id="cell-{{$i}}x{{$j}}">
+                    bau
+                  </td>
+                @endfor
+            </tr>
+          @endfor
+      </table>
 </body>
 </html>
--- tests/MinesweeperTest.php
+++ tests/MinesweeperTest.php
@@ -13,4 +13,26 @@ class MinesweeperTest extends TestCase
         ->see('Minesweeper');
     }
 
+    public function testColumn11()
+    {
+        $this->visit('/minesweeper')
+        ->see('Minesweeper')
+        ->see('cell-1x1');
+
+    }
+
+    public function testColumn()
+    {
+        $this->visit('/minesweeper')
+        ->see('Minesweeper');
+        for($i=1;$i<=8;$i++)
+        {
+          for($j=1;$j<=8;$j++)
+          {
+            $this->see('cell-'.$i.'x'.$j);
+          }
+        }
+
+    }
+
 }
