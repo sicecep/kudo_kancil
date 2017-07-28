00de192f5c86de90b071d527fc196048129b2c9f
Ichsan Rahardianto <ichsan.rahardianto@gmail.com>
2017-07-28 11:00:01 +0700

    added tests

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -9,12 +9,97 @@ class PrimeFactorsTest extends TestCase
      */
     public function test16()
     {
-        $inputNumber = 16;
+        $nbr = 16;
+        $factor = [2,2,2,2];
 
-        $this->visit('/primeFactors?number='.$inputNumber)
-              ->seeJson([
-                 'number' => $inputNumber,
-                 'decomposition' => [2,2,2,2],
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test64()
+    {
+        $nbr = 64;
+        $factor = [2,2,2,2,2,2];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test9()
+    {
+        $nbr = 9;
+        $factor = [3,3];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test14()
+    {
+        $nbr = 14;
+        $factor = [2,7];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test35()
+    {
+        $nbr = 35;
+        $factor = [5,7];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test49()
+    {
+        $nbr = 49;
+        $factor = [7,7];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test69()
+    {
+        $nbr = 69;
+        $factor = [3,23];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
+              ]);
+    }
+
+    public function test100()
+    {
+        $nbr = 100;
+        $factor = [2,2,5,5];
+
+        $this->get('/primeFactors?number='.$nbr)
+             ->seeJson([
+                 'number' => strval($nbr),
+                 'decomposition' => $factor
               ]);
     }
 }
