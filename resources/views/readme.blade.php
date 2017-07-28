4fdb7a76fd4c81f535eab70ef7f4414ddc06410f
Muhammad Ihsan <muhammad.ihsan@kudo.co.id>
2017-07-28 16:54:39 +0700

    add docker

--- resources/views/readme.blade.php
+++ resources/views/readme.blade.php
@@ -4,7 +4,7 @@
 <body>
       YoseTheGame
       <input type="text" id="ship"><button id="dock" onclick="submit()">dock</button>
-      <a href="#" id="astroport-name">astroport-name</a>
+      <a href="#" id="astroport-name" name="ship">astroport-name</a>
       <div id="gate-1">first gate<div id="ship-1"></div></div>
       <div id="gate-2">second gate<div id="ship-2">the name of the ship docked at gate 2</div></div>
       <div id="gate-3">third gate<div id="ship-3">the name of the ship docked at gate 3</div></div>
